b231e273c60d9b9b936feafc242e3fcdf5c3ec5f
Andre Tavares <andreltmj@gmail.com>
2021-09-25 22:05:28 -0400

    Modificando App terceira vez

--- test.py
+++ test.py
@@ -19,7 +19,7 @@ class Test(unittest.TestCase):
         result = self.app.get('/') 
 
         # verifica o retorno do conteudo da pagina
-        self.assertRegex(result.data.decode(), "Pipeline DevOps.")
+        self.assertRegex(result.data.decode(), "Pipeline DevOps Andre.")
 
 
 if __name__ == "__main__":
